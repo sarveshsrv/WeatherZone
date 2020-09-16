e806cc6670b7e38f68708e528868896bdcfa2f5a
Sarvesh Tiwari <sarveshsandilya01@gmail.com>
2020-09-16 18:21:22 +0530

    Location Screen Updated

--- lib/screens/location_screen.dart
+++ lib/screens/location_screen.dart
@@ -50,7 +50,7 @@ class _LocationScreenState extends State<LocationScreen> {
       body: Container(
         decoration: BoxDecoration(
           image: DecorationImage(
-            image: AssetImage('images/location_background.jpg'),
+            image: AssetImage('images/forest.jpg'),
             fit: BoxFit.cover,
             colorFilter: ColorFilter.mode(
                 Colors.white.withOpacity(0.8), BlendMode.dstATop),
