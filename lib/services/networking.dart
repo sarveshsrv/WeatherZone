1d8259cb12c1cc6dc7c5346bb0a384f2ee3292b6
Sarvesh Tiwari <sarveshsandilya01@gmail.com>
2020-09-16 18:28:43 +0530

    API integrated

--- lib/services/networking.dart
+++ lib/services/networking.dart
@@ -1,20 +1,20 @@
-// import 'package:http/http.dart' as http;
-// import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
-// class NetworkHelper {
-//   NetworkHelper(this.url);
+class NetworkHelper {
+  NetworkHelper(this.url);
 
-//   final String url;
+  final String url;
 
-//   Future getData() async {
-//     http.Response response = await http.get(url);
+  Future getData() async {
+    http.Response response = await http.get(url);
 
-//     if (response.statusCode == 200) {
-//       String data = response.body;
+    if (response.statusCode == 200) {
+      String data = response.body;
 
-//       return jsonDecode(data);
-//     } else {
-//       print(response.statusCode);
-//     }
-//   }
-// }
+      return jsonDecode(data);
+    } else {
+      print(response.statusCode);
+    }
+  }
+}
