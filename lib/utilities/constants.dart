6b332346b0d42cb4ad04203ad3df36d338a6591f
Sarvesh Tiwari <sarveshsandilya01@gmail.com>
2020-09-27 20:46:48 +0530

    Constant updated

--- lib/utilities/constants.dart
+++ lib/utilities/constants.dart
@@ -2,12 +2,12 @@ import 'package:flutter/material.dart';
 
 const kTempTextStyle = TextStyle(
   fontFamily: 'Spartan MB',
-  fontSize: 100.0,
+  fontSize: 80.0,
 );
 
 const kMessageTextStyle = TextStyle(
   fontFamily: 'Spartan MB',
-  fontSize: 60.0,
+  fontSize: 50.0,
 );
 
 const kButtonTextStyle = TextStyle(
@@ -23,7 +23,7 @@ const kTextFieldInputDecoration = InputDecoration(
   filled: true,
   fillColor: Colors.white,
   icon: Icon(
-    Icons.location_city,
+    Icons.location_on,
     color: Colors.white,
   ),
   hintText: 'Enter City Name',
